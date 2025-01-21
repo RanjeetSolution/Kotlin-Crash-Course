574eaab849550026250f9e7b4876a73889fb7065
Ranjeet <infoit62@gmail.com>
2025-01-21 18:25:59 +0530

    Generic

--- src/main/kotlin/Ch31_AnyAndNothing.kt
+++ src/main/kotlin/Ch31_AnyAndNothing.kt
@@ -1,6 +1,23 @@
+import javax.sound.midi.MetaMessage
+
 //Any: Used to represent any type of value, parents of all types
 //Nothing: USed to represent a value that does not exist, Subtype/child of all types
 
 fun main() {
 
+    val obj: Any = "Hello"
+
+    val str1: String? = obj as? String
+    val str2: String = obj as String
+
+    val result = try {
+        fail("any erroe")
+    }catch (e: IllegalArgumentException){
+        println(e.stackTrace)
+    }
+
+}
+
+fun fail(message: String): Nothing {
+    throw IllegalArgumentException(message)
 }
